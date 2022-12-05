330b6d158307e40d29ed6291ed96caeb95c277d7
Bitarius <bitarius@gmail.com>
2022-12-05 18:25:05 +0300

    Homework one

---
+++ Task002/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+*/
+
+Console.WriteLine("Введите первое число");
+int numberOne = int.Parse(Console.ReadLine() ??"");
+
+Console.WriteLine("Введите второе число");
+int numberTwo = int.Parse(Console.ReadLine() ??"");
+
+if (numberOne > numberTwo)
+{
+    int max = numberOne;
+    Console.WriteLine($"Первое число {numberOne} больше второго {numberTwo}");
+}
+else if (numberTwo > numberOne)
+{
+    int max = numberTwo;
+    Console.WriteLine($"Второе число {numberTwo} больше первого {numberOne}");
+}
+else if (numberOne == numberTwo)
+{
+    Console.WriteLine ("Эти числа равны");
+}
---
+++ Task004/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+*/
+
+Console.WriteLine("Введите первое число");
+int numberOne = int.Parse(Console.ReadLine() ??"");
+
+Console.WriteLine("Введите второе число");
+int numberTwo = int.Parse(Console.ReadLine() ??"");
+
+Console.WriteLine("Введите третье число");
+int numberThree = int.Parse(Console.ReadLine() ??"");
+
+int maxNumber = numberOne;
+
+if (maxNumber < numberTwo)
+{
+    maxNumber = numberTwo;
+}
+
+if (maxNumber < numberThree)
+{
+    maxNumber = numberThree;
+}
+
+Console.WriteLine(maxNumber);
